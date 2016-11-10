76b2f2609af5cdd05fffc167ef129ea0f6b9f50f
Chatuporn.B <Chatuporn.B@10.40.3.99>
2016-11-10 23:07:30 +0700

    แก้ไข layout

--- views/site/index.php
+++ views/site/index.php
@@ -6,6 +6,8 @@ $baseUriCss = $baseUri.'/assets/theme';
 <!--Feature Posts Here-->
 <section class="row featured_posts">
 	<div class="container-fluid">
+	
+		<?php for ($i=1;$i<=6;$i++){?>
 		<div class="row" id="featured_posts">
 			<div class="col-lg-6 col-md-6 post">
 				<div class="row inner">
@@ -67,197 +69,12 @@ $baseUriCss = $baseUri.'/assets/theme';
 			
 		</div>
 		
+		<?php }?>
 		
-		<div class="row" id="featured_posts">
-			
-			
-			<div class="col-lg-6 col-md-6 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/2.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category sports">
-							<a href="#">sports</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Philip Rivers: I’m going to be a
-								Charger, wherever we are</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/2.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category sports">
-							<a href="#">sports</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Philip Rivers: I’m going to be a
-								Charger, wherever we are</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			
-			<div class="col-lg-6 col-md-6 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/1.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category politics">
-							<a href="#">politics</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">U.S. House Chamber Closed After
-								Unknown Material</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			
-			
-		</div>
 
-		<div class="row" id="featured_posts">
-			
-			<div class="col-lg-3 col-md-3 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/3.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category money">
-							<a href="#">money</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">U.S. Flag Raised Over Embassy in
-								Republic of Cuba</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-3 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/4.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category world">
-							<a href="#">world</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Death Toll in Chinese Port Blasts
-								Rises to 104</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			
-			<div class="col-lg-6 col-md-6 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/2.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category sports">
-							<a href="#">sports</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Philip Rivers: I’m going to be a
-								Charger, wherever we are</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			
-			
-			
-			
-		</div>
-
-		<div class="row" id="featured_posts">
 		
-			<div class="col-lg-6 col-md-6 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/2.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category sports">
-							<a href="#">sports</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Philip Rivers: I’m going to be a
-								Charger, wherever we are</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-3 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/3.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category money">
-							<a href="#">money</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">U.S. Flag Raised Over Embassy in
-								Republic of Cuba</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3 col-md-3 post">
-				<div class="row inner">
-					<img src="<?= $baseUriCss?>/images/posts/features/4.jpg" alt="" class="featured-img">
-					<div class="row m0 content">
-						<h4 class="category world">
-							<a href="#">world</a>
-						</h4>
-						<h2 class="title">
-							<a href="single-blog.html">Death Toll in Chinese Port Blasts
-								Rises to 104</a>
-						</h2>
-						<ul class="post_meta nav nav-pills">
-							<li><a href="#">August 6, 2015</a></li>
-							<li><a href="#"><i class="fa fa-thumbs-o-up"></i>1456</a></li>
-							<li><a href="#"><i class="fa fa-commenting"></i>32</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			
-		</div>
+
+		
 		
 		
 	</div>
